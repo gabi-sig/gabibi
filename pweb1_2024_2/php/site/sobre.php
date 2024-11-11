da0fa8b16e4c3f8f2f8c2a1aa1b575ebefac170e
gabi-sig <gabriela.s2006@aluno.ifsc.edu.br>
2024-11-11 17:19:44 -0300

    eemplo ula php

---
+++ pweb1_2024_2/php/site/sobre.php
@@ -0,0 +1,16 @@
+<?php
+    include "./header.php";
+?>
+
+<div class="row">
+    <div class="col">
+        <h2>Sobre</h2>
+        <p class="text-center">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident fugiat voluptatem rem? Neque, obcaecati architecto. Voluptatum dolorem, quasi aperiam magni dolores magnam at corrupti, deleniti dicta rem pariatur perspiciatis unde?
+        </p>
+    </div>
+</div>
+
+<?php
+    include "./footer.php";
+?>
